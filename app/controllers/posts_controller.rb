ea688b6941ed6726ced7048eedd19dde964e34e8
syougo374 <supernaturalsyougo@gmail.com>
2021-03-26 08:36:13 +0000

    ID引き渡しができない

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,53 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
+    @post = Post.all
   end
-
   def new
     @post = Post.new
   end
-
   def create
-    @post = Post.create(content: params[:posts][:content])
-    redirect_to new_post_path
+    @post = Post.new(posts_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "投稿を作成しました！"
+      else
+        render :new
+      end
+    end
+  end
+  def show
+  
+  end
+
+  def edit 
+ 
+  end
+  def update 
+
+  if @post.update(posts_params)
+    redirect_to posts_path, notice: "投稿を更新しました"
+  else 
+    render :edit
+  end
+
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました!"
+  end
+  def confirm
+    @post = Post.new(posts_params)
+    render :new if @post.invalid?
   end
 
   private
   def post_params
-    params.require(:posts).permit(:content)
+    params.require(:post).permit(:content)
   end
-
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
+  validates :content, presence: true
 end
