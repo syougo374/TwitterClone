0eafb96d57300f90e9c5f1e7584d4da25bd4ffa2
syougo374 <supernaturalsyougo@gmail.com>
2021-03-30 15:24:16 +0900

    インデント、フッター修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,18 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
   def index
     @post = Post.all
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(posts_params)
     if params[:back]
-      render :new
+       render :new
     else
       if @post.save
         redirect_to posts_path, notice: "投稿を作成しました！"
@@ -26,11 +29,11 @@ class PostsController < ApplicationController
   end
 
   def update 
-  if @post.update(posts_params)
-    redirect_to posts_path, notice: "投稿を更新しました"
-  else 
-    render :edit
-  end
+    if @post.update(posts_params)
+      redirect_to posts_path, notice: "投稿を更新しました"
+    else 
+      render :edit
+    end
   end
 
   def destroy
@@ -48,7 +51,7 @@ class PostsController < ApplicationController
   def posts_params
     params.require(:post).permit(:content)
   end
-  
+
   def set_post
     @post = Post.find(params[:id])
   end
