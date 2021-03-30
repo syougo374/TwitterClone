517baaaba119e20b50fa2cc3be5471b1f54257f7
syougo374 <supernaturalsyougo@gmail.com>
2021-03-30 14:27:40 +0900

    最終修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,35 +18,37 @@ class PostsController < ApplicationController
       end
     end
   end
-  def show
-  
+
+  def show  
   end
 
   def edit 
- 
   end
-  def update 
 
+  def update 
   if @post.update(posts_params)
     redirect_to posts_path, notice: "投稿を更新しました"
   else 
     render :edit
   end
-
   end
+
   def destroy
     @post.destroy
     redirect_to posts_path, notice: "投稿を削除しました!"
   end
+
   def confirm
     @post = Post.new(posts_params)
     render :new if @post.invalid?
   end
 
   private
+
   def posts_params
     params.require(:post).permit(:content)
   end
+  
   def set_post
     @post = Post.find(params[:id])
   end
